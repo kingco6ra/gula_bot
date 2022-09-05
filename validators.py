c57f057fe85b3a7b4c738b75be2abe4a6ce7cb95
Максим Осипов <m.osipov@ideco.ru>
2022-09-05 14:11:09 +0500

    fix time

--- validators.py
+++ validators.py
@@ -7,14 +7,14 @@ def validate_time_command(command: list) -> tuple[bool, str, str | None]:
     default_time = '09:00'
     if command[0] == '/notify' and len(command) == 1:
         return True, default_time, None
-
+    time = command[1]
     try:
-        hrs, mins = map(lambda x: int(x), command[1].split(':'))
+        hrs, mins = map(lambda x: int(x), time.split(':'))
     except ValueError:
         return False, default_time, 'Неправильный формат времени.'
     if len(command) > 2:
         return False, default_time, 'Неправильный формат сообщения.'
     elif hrs > 24 or mins > 60:
         return False, default_time, 'Неправильный формат времени.'
-    return True, f'{hrs}:{mins}', None
+    return True, time, None
 
