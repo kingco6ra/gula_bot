d0cfd0b3223e6a82cff9632d395313baa4b2ab48
Максим Осипов <m.osipov@ideco.ru>
2022-09-08 18:06:40 +0500

    перевод на асинхронщину

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 from datetime import datetime
@@ -5,7 +6,7 @@ from sqlite3 import OperationalError
 from subprocess import run
 from time import sleep
 
-import telebot
+from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
 from db_conn import insert_menu, get_menu, create_week_table
@@ -14,7 +15,7 @@ from parse import parse_menu
 from validators import validate_time_command
 from google_api import make_order, clean_orders
 
-bot = telebot.TeleBot(TELEBOT_TOKEN)
+bot = AsyncTeleBot(TELEBOT_TOKEN)
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 weekdays = {
@@ -27,11 +28,11 @@ weekdays = {
 
 
 @bot.message_handler(commands=['start'])
-def get_starting(message: Message):
-    bot.send_message(message.chat.id, parse_mode='HTML', text='Для включения напоминаний о заказе еды введите: <pre>/notify</pre>'
-                                                              'Время упоминания по умолчанию будет 08:30. Если вы хотите изменить время,'
-                                                              ' то введите: <pre>/notify 05:00</pre>')
-    bot.send_message(message.chat.id, text=f'Ссылка на таблицу для ручного оформления заказа: https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/')
+async def get_starting(message: Message):
+    await bot.send_message(message.chat.id, parse_mode='HTML', text='Для включения напоминаний о заказе еды введите: <pre>/notify</pre>'
+                                                                    'Время упоминания по умолчанию будет 08:30. Если вы хотите изменить время,'
+                                                                    ' то введите: <pre>/notify 05:00</pre>')
+    await bot.send_message(message.chat.id, text=f'Ссылка на таблицу для ручного оформления заказа: https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/')
 
 
 @bot.message_handler(commands=['notify'])
@@ -70,36 +71,36 @@ def enable_notify(message: Message):
 
 
 @bot.message_handler(commands=['order'])
-def food_ordering(message: Message):
+async def food_ordering(message: Message):
     full_string = message.text.split()
     full_name = f'{full_string[1]} {full_string[2]}'
 
     status, msg = make_order(full_name, message.text.split('\n')[1:])
     if msg:
-        bot.send_message(message.chat.id, *msg)
+        await bot.send_message(message.chat.id, *msg)
 
     if status:
-        bot.send_message(message.chat.id, f'Заказ произведен успешно. Не забудьте произвести оплату.\n'
-                                          f'Таблица заказов: https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/')
+        await bot.send_message(message.chat.id, f'Заказ произведен успешно. Не забудьте произвести оплату.\n'
+                                                f'Таблица заказов: https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/')
     else:
-        bot.send_message(message.chat.id, 'Возникли проблемы при заполнении таблицы.')
+        await bot.send_message(message.chat.id, 'Возникли проблемы при заполнении таблицы.')
 
 
 @bot.message_handler(commands=['help'])
-def get_help(message: Message):
-    bot.send_message(message.chat.id, parse_mode='HTML', text=f'Как сделать заказ:\n'
-                                                              '<pre>'
-                                                              '/order Пупкин В.\n'
-                                                              'ПНД: мясо, курица, рыба\n'
-                                                              'ЧТВ: суп, второе, салат'
-                                                              '</pre>'
-                                                              'И я автоматически запишу заказ в таблицу:')
-    with open('src/help.jpg', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
+async def get_help(message: Message):
+    await bot.send_message(message.chat.id, parse_mode='HTML', text=f'Как сделать заказ:\n'
+                                                                    '<pre>'
+                                                                    '/order Пупкин В.\n'
+                                                                    'ПНД: мясо, курица, рыба\n'
+                                                                    'ЧТВ: суп, второе, салат'
+                                                                    '</pre>'
+                                                                    'И я автоматически запишу заказ в таблицу:')
+    async with open('src/help.jpg', 'rb') as photo:
+        await bot.send_photo(message.chat.id, photo)
 
 
 @bot.message_handler(commands=['menu'])
-def get_week_menu(message: Message):
+async def get_week_menu(message: Message):
     """
     /order - получить сегодняшнее меню
     /order ПНД - получить меню на понедельник
@@ -108,48 +109,52 @@ def get_week_menu(message: Message):
     """
     today = weekdays.get(datetime.now().isoweekday())
     table_name = f'menu_{message.chat.id}_{datetime.now().isocalendar()[1]}'.replace('-', '')
-    if message.text == '/menu':
-        menu = '\n'.join(get_menu(today, table_name))
-        bot.send_message(message.chat.id, parse_mode='HTML', text=f'Меню на сегодня:\n\n'
-                                                                  f'<pre>{menu}</pre>')
-    elif message.text.split()[1].upper() in weekdays.values():
-        day = message.text.split()[1].upper()
-        menu = '\n'.join(get_menu(day, table_name))
-        bot.send_message(message.chat.id, parse_mode='HTML', text=f'Меню на {day}:\n\n'
-                                                                  f'<pre>{menu}</pre>')
-    else:
-        bot.send_message(message.chat.id, parse_mode='HTML', text=f'Неправильный формат команды. Убедитесь в том, что вы правильно ввели команду:\n'
-                                                                  f'<pre>/menu $DAY - где $DAY: ПТН, ВТ, СР, ЧТВ, ПНД</pre>'
-                                                                  f'Для того чтобы получить меню на сегодня: <pre>/menu</pre>')
+    try:
+        if message.text == '/menu':
+            menu = '\n'.join(get_menu(today, table_name))
+            await bot.send_message(message.chat.id, parse_mode='HTML', text=f'Меню на сегодня:\n\n'
+                                                                            f'<pre>{menu}</pre>')
+        elif message.text.split()[1].upper() in weekdays.values():
+            day = message.text.split()[1].upper()
+            menu = '\n'.join(get_menu(day, table_name))
+            await bot.send_message(message.chat.id, parse_mode='HTML', text=f'Меню на {day}:\n\n'
+                                                                            f'<pre>{menu}</pre>')
+        else:
+            await bot.send_message(message.chat.id, parse_mode='HTML', text=f'Неправильный формат команды. Убедитесь в том, что вы правильно ввели команду:\n'
+                                                                            f'<pre>/menu $DAY - где $DAY: ПТН, ВТ, СР, ЧТВ, ПНД</pre>'
+                                                                            f'Для того чтобы получить меню на сегодня: <pre>/menu</pre>')
+    except OperationalError:
+        await bot.send_message(message.chat.id, f'Меню для текущей недели не было загружено.')
 
 
 @bot.message_handler(commands=['update'])
-def update(message: Message):
-    bot.send_message(message.chat.id, 'Начинаю обновление...')
+async def update(message: Message):
+    await bot.send_message(message.chat.id, 'Начинаю обновление...')
     log.info(run(['git', 'restore', '.']))
     log.info(run(['git', 'fetch']))
     log.info(run(['git', 'pull']).stdout)
     log.info(run(['cp', 'gula-bot.service', '/etc/systemd/system/']))
-    bot.send_message(message.chat.id, 'Обновление выполнено успешно!')
+    await bot.send_message(message.chat.id, 'Обновление выполнено успешно!')
     log.info(run(['systemctl', 'restart', 'gula-bot']))
 
 
 @bot.message_handler(content_types=['text'])
-def food_is_comming(message: Message):
+async def food_is_comming(message: Message):
     """Хэндлер фраз-крючков, по нахождению которых - желаем приятного аппетита"""
     hook_words = {'поднимается', 'приехал', 'приехала', 'примите', 'привезли'}
     lower_message = set(map(lambda x: x.lower(), message.text.split(' ')))
     if set(lower_message).intersection(hook_words):
         with open('src/eating.gif', 'rb') as gif:
-            bot.send_message(message.chat.id, 'Приятного аппетита.')
-            bot.send_animation(message.chat.id, gif)
+            await bot.send_message(message.chat.id, 'Приятного аппетита.')
+            await bot.send_animation(message.chat.id, gif)
 
 
 @bot.message_handler(content_types=['document'])
-def get_new_week_menu(message: Message):
+async def get_new_week_menu(message: Message):
     """Скачиваем и парсим XLSX чтобы получить еженедельное меню в TXT формате"""
     menu_dir = f'{os.getcwd()}/src/menus/{message.document.file_name}'
-    document = bot.download_file(bot.get_file(message.document.file_id).file_path)
+    file = await bot.get_file(message.document.file_id)
+    document = await bot.download_file(file.file_path)
     table_name = f'menu_{message.chat.id}_{datetime.now().isocalendar()[1]}'.replace('-', '')
     with open(menu_dir, 'wb') as menu:
         menu.write(document)
@@ -157,11 +162,25 @@ def get_new_week_menu(message: Message):
         create_week_table(table_name)
         menu = parse_menu(menu_dir)
         insert_menu(menu, table_name)
-        bot.send_message(message.chat.id, 'Меню было успешно занесено в базу данных.')
+        await bot.send_message(message.chat.id, 'Меню было успешно занесено в базу данных.')
     except OperationalError:
-        bot.send_message(message.chat.id, 'Меню для этой недели уже загружено в базу данных.')
+        await bot.send_message(message.chat.id, 'Меню для этой недели уже загружено в базу данных.')
+
+
+async def notify():
+    while True:
+        print('hello')
+        await asyncio.sleep(5)
+
+
+async def main():
+    tasks = [
+        bot.infinity_polling(logger_level=logging.INFO),
+        notify(),
+    ]
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
     log.info('bot was been started')
-    bot.infinity_polling()
+    asyncio.run(main())
